04b3c4927e8451789ce358c9004282e429b10269
Tarde <Tarde@PC14-T3INF>
2022-03-24 17:14:51 +0100

    Los últimos cambios se han realizado correctamente.

---
+++ src/proyectojava/ProyectoJava.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectojava;
+
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+/**
+ *
+ * @author Tarde
+ */
+public class ProyectoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+   static void miMetodo(String nombre, int edad) {
+ System.out.println(nombre + " tiene " + edad);
+ }
+public static void main(String[] args) {
+ // Lo que sigue es la lógica de mi aplicación
+int k = 0;
+do {
+ System.out.println("Iteración do-while: " + k);
+ k++;
+}
+while (k < 5);
+ // Invocar varias veces el método creado al inicio
+ miMetodo("Luís", 4);
+ miMetodo("Martina", 6);
+ miMetodo("Ángel", 25);
+ miMetodo("Jenny", 15);
+ miMetodo("José Antonio", 31);
+}
+    
+}
